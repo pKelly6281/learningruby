dea6a5df55ce213967c1ff9720dfc60308d8eca9
Patrick Kelly <pk6281@mcla.edu>
2014-10-14 14:32:14 -0400

    Examples 2 & 3

---
+++ ex3.rb
@@ -0,0 +1,30 @@
+
+
+puts "I will now count my chickens"
+
+#this line of code below says the # of hens
+puts "Hens #{25 + 30 / 6}"
+#this line of code below says the # of roosters
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs"
+
+#this line of code adds, divides, modulates numbers
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7"
+
+puts 3 + 2 < 5 - 7
+
+
+#the following two lines of code below add two numbers and display them to the terminal
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more"
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
