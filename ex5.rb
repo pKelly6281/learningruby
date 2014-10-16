3ebde008243daba20c9a26cebf443b50dbf0cde9
Patrick Kelly <pk6281@mcla.edu>
2014-10-16 11:34:17 -0400

    Adding ex5 to Git Repo

---
+++ ex5.rb
@@ -0,0 +1,22 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie in 2009
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair}."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, #{my_weight} I get #{my_age+my_weight+my_height}."
+
+my_cent = my_height * 2.54
+my_KG = my_weight / 2.20462
+
+puts "my_height in cm is #{my_cent}"
+puts "my_weight in kg is #{my_KG}"
