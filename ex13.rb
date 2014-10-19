65a7e4235df74ebe222c0c698aad7d9495eb0dc0
Patrick Kelly <pk6281@mcla.edu>
2014-10-19 15:13:08 -0400

    Example 14 Pat

--- ex13.rb
+++ ex13.rb
@@ -5,9 +5,9 @@ puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
 
 print "What is your name?: "
-name = gets.chomp
+name = $stdin.gets.chomp
 
 print "Change first:"
-first = gets.chomp
+first = $stdin.gets.chomp
 
 puts "Hello #{name} #{first}"
