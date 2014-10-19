3032a76ce1cd22b85cecf7b88b599f0834729f60
Patrick Kelly <pk6281@mcla.edu>
2014-10-19 17:31:18 -0400

    adding ex25

---
+++ ex25.rb
@@ -0,0 +1,44 @@
+module Ex25
+
+	#break the words for us
+	def Ex25.break_words(stuff)
+		words = stuff.split(' ')
+		return words
+	end
+	#sort the words
+	def Ex25.sort_words(words)
+		return words.sort
+	end
+
+	#print the first word after shifting it off
+	def Ex25.print_first_word(words)
+		word = words.shift
+		puts word
+	end
+
+	#prints the last word after popping it off
+	def Ex25.print_last_word(words)
+		word = words.pop
+		puts word
+	end
+
+	#takes in a full sentence and returns the sorted words
+	def Ex25.sort_sentence(sentence)
+		words = Ex25.break_words(sentence)
+		return Ex25.sort_words(words)
+	end
+
+def Ex25.print_first_and_last(sentence)
+    words = Ex25.break_words(sentence)
+    Ex25.print_first_word(words)
+    Ex25.print_last_word(words)
+  end
+
+	#prints the first and last words of the sentence
+	def Ex25.print_first_and_sorted(sentence)
+		words = Ex25.sort_sentence(sentence)
+		Ex25.print_first_word(words)
+		Ex25.print_last_word(words)
+	end
+
+end
