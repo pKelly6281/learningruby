c8a81794a0ac7aea38e830db42e87a661a2c7b6e
Patrick Kelly <pk6281@mcla.edu>
2014-10-19 15:42:10 -0400

    ex16 Patrick

--- ex15.rb
+++ ex15.rb
@@ -5,8 +5,4 @@ txt = open(filename)
 puts "Here's your file #{filename}"
 print txt.read
 
-print "\nType the filename again: "
-file_again = $stdin.gets.chomp
-txt_again = open(file_again)
-
-print txt_again.read
+txt.close()
